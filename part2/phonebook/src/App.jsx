3a377537decfd06962ad1c43a4cde94509af9ddd
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-02-16 20:53:00 +0400

    Solution for 2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [number, setNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const handleNameChange = (event) => {
     // console.log(event.target.value);
@@ -13,8 +17,17 @@ const App = () => {
   };
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
     setNumber(event.target.value);
+  };
+
+  const checkOverlap = (person) => {
+    const filtered = person.name.toLowerCase().includes(search.toLowerCase());
+    return filtered;
+  };
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
   };
 
   const addName = (event) => {
@@ -34,7 +47,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input type="text" value={search} onChange={handleSearch} />
+      </div>
       <form onSubmit={addName}>
+        <h2>Add a new</h2>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
@@ -46,11 +64,14 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name}>
-          {person.name} {person.number}
-        </div>
-      ))}
+      {persons.filter(checkOverlap).map((person) => {
+        console.log("Filtered Person:", person);
+        return (
+          <div key={person.name}>
+            {person.name} {person.number}
+          </div>
+        );
+      })}
     </div>
   );
 };
